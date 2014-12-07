4257f019c34071b2af021c20c0c9536dae6c079f
James Tauber <jtauber@jtauber.com>
2014-12-07 17:56:14 -0500

    check point

--- verbs.py
+++ verbs.py
@@ -1,30 +1,43 @@
 from accentuation import recessive
 from endings import PRIMARY_ACTIVE, PRIMARY_MIDDLE, SECONDARY_ACTIVE, SECONDARY_MIDDLE
+from endings import ACTIVE_SUBJUNCTIVE, MIDDLE_SUBJUNCTIVE
+from endings import ACTIVE_OPTATIVE, MIDDLE_OPTATIVE
+from endings import PRIMARY_ACTIVE_IMPERATIVE, PRIMARY_MIDDLE_IMPERATIVE
+from endings import ACTIVE_INFINITIVE, MIDDLE_INFINITIVE
 from utils import remove, has_accent, remove_length
 
 
 def ENDINGS(paradigm):
 
-    def forward(stems, pn):
+    def forward(stems, pn=None):
         forms = []
 
         for stem in stems:
-            for ending, stem_ending in paradigm[pn]:
+            if pn:
+                pp = paradigm[pn]
+            else:
+                pp = paradigm
+            for ending, stem_ending in pp:
                 stem2, stem_ending = (stem[:-len(stem_ending)], remove(stem_ending)) if stem_ending else (stem, "")
                 if stem.endswith(stem_ending):
                     forms.append(stem2 + ending)
 
         return forms
 
-    def reverse(form, pn):
-        # print("@1", form, pn)
+    def reverse(form, pn=None):
         stems = []
-        if pn in paradigm:
-            endings = paradigm[pn]
-            for ending, stem_ending in endings:
-                # print("@2", ending, stem_ending)
-                if form.endswith(remove_length(ending)):
-                    stems.append(form[:-len(ending)] + (stem_ending if stem_ending else ""))
+
+        if pn:
+            if pn in paradigm:
+                endings = paradigm[pn]
+            else:
+                endings = []
+        else:
+            endings = paradigm
+
+        for ending, stem_ending in endings:
+            if form.endswith(remove_length(ending)):
+                stems.append(form[:-len(ending)] + (stem_ending if stem_ending else ""))
 
         if stems:
             return stems
@@ -38,6 +51,17 @@ primary_active, rev_primary_active = ENDINGS(PRIMARY_ACTIVE)
 primary_middle, rev_primary_middle = ENDINGS(PRIMARY_MIDDLE)
 secondary_active, rev_secondary_active = ENDINGS(SECONDARY_ACTIVE)
 secondary_middle, rev_secondary_middle = ENDINGS(SECONDARY_MIDDLE)
+
+active_subjunctive, rev_active_subjunctive = ENDINGS(ACTIVE_SUBJUNCTIVE)
+middle_subjunctive, rev_middle_subjunctive = ENDINGS(MIDDLE_SUBJUNCTIVE)
+active_optative, rev_active_optative = ENDINGS(ACTIVE_OPTATIVE)
+middle_optative, rev_middle_optative = ENDINGS(MIDDLE_OPTATIVE)
+
+primary_active_imperative, rev_primary_active_imperative = ENDINGS(PRIMARY_ACTIVE_IMPERATIVE)
+primary_middle_imperative, rev_primary_middle_imperative = ENDINGS(PRIMARY_MIDDLE_IMPERATIVE)
+
+active_infinitive, rev_active_infinitive = ENDINGS(ACTIVE_INFINITIVE)
+middle_infinitive, rev_middle_infinitive = ENDINGS(MIDDLE_INFINITIVE)
 
 
 def PART(stem_key):
@@ -74,6 +98,23 @@ class Verb:
     def FMI(self, pn): return primary_middle(future(self), pn)
     def FPI(self, pn): return primary_middle(future_passive(self), pn)
 
+    def PAS(self, pn): return active_subjunctive(present(self), pn)
+    def PMS(self, pn): return middle_subjunctive(present(self), pn)
+    def PAO(self, pn): return active_optative(present(self), pn)
+    def PMO(self, pn): return middle_optative(present(self), pn)
+    def FAO(self, pn): return active_optative(future(self), pn)
+    def FMO(self, pn): return middle_optative(future(self), pn)
+    def FPO(self, pn): return middle_optative(future_passive(self), pn)
+
+    def PAD(self, pn): return primary_active_imperative(present(self), pn)
+    def PMD(self, pn): return primary_middle_imperative(present(self), pn)
+
+    def PAN(self): return active_infinitive(present(self))
+    def PMN(self): return middle_infinitive(present(self))
+    def FAN(self): return active_infinitive(future(self))
+    def FMN(self): return middle_infinitive(future(self))
+    def FPN(self): return middle_infinitive(future_passive(self))
+
     def rev_PAI(self, form, pn): return rev_present(rev_primary_active(form, pn))
     def rev_PMI(self, form, pn): return rev_present(rev_primary_middle(form, pn))
     def rev_IAI(self, form, pn): return rev_imperfect(rev_secondary_active(form, pn))
@@ -81,6 +122,23 @@ class Verb:
     def rev_FAI(self, form, pn): return rev_future(rev_primary_active(form, pn))
     def rev_FMI(self, form, pn): return rev_future(rev_primary_middle(form, pn))
     def rev_FPI(self, form, pn): return rev_future_passive(rev_primary_middle(form, pn))
+
+    def rev_PAS(self, form, pn): return rev_present(rev_active_subjunctive(form, pn))
+    def rev_PMS(self, form, pn): return rev_present(rev_middle_subjunctive(form, pn))
+    def rev_PAO(self, form, pn): return rev_present(rev_active_optative(form, pn))
+    def rev_PMO(self, form, pn): return rev_present(rev_middle_optative(form, pn))
+    def rev_FAO(self, form, pn): return rev_future(rev_active_optative(form, pn))
+    def rev_FMO(self, form, pn): return rev_future(rev_middle_optative(form, pn))
+    def rev_FPO(self, form, pn): return rev_future_passive(rev_middle_optative(form, pn))
+
+    def rev_PAD(self, form, pn): return rev_present(rev_primary_active_imperative(form, pn))
+    def rev_PMD(self, form, pn): return rev_present(rev_primary_middle_imperative(form, pn))
+
+    def rev_PAN(self, form): return rev_present(rev_active_infinitive(form))
+    def rev_PMN(self, form): return rev_present(rev_middle_infinitive(form))
+    def rev_FAN(self, form): return rev_future(rev_active_infinitive(form))
+    def rev_FMN(self, form): return rev_future(rev_middle_infinitive(form))
+    def rev_FPN(self, form): return rev_future_passive(rev_middle_infinitive(form))
 
 
 def conditional_recessive(word):
@@ -94,8 +152,11 @@ def conditional_recessive(word):
 
 
 def calculate_form(lexeme, parse):
-    tvm, pn = parse.split(".")
-    result = getattr(lexeme, tvm)(pn)
+    if parse[2] == "N":
+        result = getattr(lexeme, parse)()
+    else:
+        tvm, pn = parse.split(".")
+        result = getattr(lexeme, tvm)(pn)
 
     if isinstance(result, list):
         result = [conditional_recessive(x) for x in result]
@@ -105,6 +166,10 @@ def calculate_form(lexeme, parse):
 
 
 def calculate_part(lexeme, form, parse):
-    tvm, pn = parse.split(".")
-    stem_key, results = getattr(lexeme, "rev_" + tvm)(form, pn)
+    if parse[2] == "N":
+        stem_key, results = getattr(lexeme, "rev_" + parse)(form)
+    else:
+        tvm, pn = parse.split(".")
+        stem_key, results = getattr(lexeme, "rev_" + tvm)(form, pn)
+
     return stem_key, [remove(result) for result in results]
